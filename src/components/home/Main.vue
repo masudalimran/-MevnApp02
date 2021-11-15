b3821238a35a22e99b39adc5ea1acbac05fea155
masudalimran <masudalimran92@gmail.com>
2021-11-15 11:58:31 +0600

    update

---
+++ src/components/home/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+
+ 
+<script>
+export default {
+  name: "Main",
+};
+</script>
+  
+<style lang='sass' scoped>
+</style>
---
+++ src/routes/routes.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Main from '../components/home/Main.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    mode: 'history',
+    routes: [{
+        path: '/',
+        name: 'Main',
+        component: Main
+    }]
+})
